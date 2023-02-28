ae320d422e9c14bef1aa26642322da29fc73b14f
Zong Yuan Lee <zy.lee@nettium.net>
2023-02-28 17:31:00 +0800

    test

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,39 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using TodoAPI.Interface;
 using TodoAPI.Model;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Mvc;
 
 namespace TodoAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class TodoController:ControllerBase
+    public class TodoController : ControllerBase
     {
         private ITodoService _TodoService;
-    
+
         public TodoController(ITodoService TodoService)
         {
             _TodoService = TodoService;
         }
 
         [HttpPost]
-        public async Task <IActionResult> addTodo([FromBody] Todo todo)
+        public async Task<IActionResult> addTodo([FromBody] Todo todo)
         {
-            var checkAdd =  await _TodoService.addTodo(todo);
+            var checkAdd = await _TodoService.addTodo(todo);
             return Ok(checkAdd);
         }
 
 
         [HttpGet]
-        public async Task <IActionResult> GetTodoList()
+        public async Task<IActionResult> GetTodoList()
         {
             var todoList = await _TodoService.getTodoList();
             return Ok(todoList);
         }
-    
+
     }
 }
